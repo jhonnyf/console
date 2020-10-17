bb7f01a5a87ad927a2aacae5a6c3694ed815e1b1
Jhonny Fialho <jhonny@hello-world.cc>
2020-10-17 13:48:19 -0300

    update de pacotes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class DashboardController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
         return view('dashboard.index');
     }
