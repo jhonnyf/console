18e24d550e37cf24e694daca665d5f98a9709508
Jhonny Fialho <jhonny@hello-world.cc>
2020-12-12 00:57:12 -0300

    modal upload

--- resources/views/files/upload-form.blade.php
+++ resources/views/files/upload-form.blade.php
@@ -1,24 +1,24 @@
 <div class="container">
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-                    <h4 class="header-title mt-0 mb-4">Upload de arquivos</h4>                        
+    <div class="card">
+        <div class="card-body">
+            <h4 class="header-title mt-0 mb-4">Upload de arquivos</h4>                                      
 
-                    <form action="{{ route('files.upload-submit', ['module' => $module, 'link_id' => $link_id, 'file_gallery_id' => $file_gallery_id]) }}" method="post" class="dropzone" id="dropzone-form">
-                        @csrf   
-                        <div class="fallback">
-                            <input name="file" type="file" multiple />
-                        </div>
-
-                        <div class="dz-message needsclick">
-                            <i class="h1 text-muted  uil-cloud-upload"></i>
-                            <h3>Solte os arquivos aqui ou clique para fazer o upload.</h3>                               
-                        </div>
-                    </form>
-
+            <form action="{{ route('files.upload-submit', ['module' => $module, 'link_id' => $link_id, 'file_gallery_id' => $file_gallery_id]) }}" method="post" class="dropzone mb-3" id="dropzone-form">
+                @csrf   
+                <div class="fallback">
+                    <input name="file" type="file" multiple />
                 </div>
+
+                <div class="dz-message needsclick">
+                    <i class="h1 text-muted  uil-cloud-upload"></i>
+                    <h3>Solte os arquivos aqui ou clique para fazer o upload.</h3>                               
+                </div>
+            </form>
+
+            <div class="text-right">
+                <button type="button" class="btn btn-dark">FECHAR</button>
             </div>
+
         </div>
     </div>
 </div>
