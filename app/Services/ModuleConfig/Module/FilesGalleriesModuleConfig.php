303398cc69eec44f7bc72c2837e61ba60170dc1e
Jhonny Fialho <jhonny@hello-world.cc>
2020-12-18 23:11:09 -0300

    FilesGalleries

--- app/Services/ModuleConfig/Module/FilesGalleriesModuleConfig.php
+++ app/Services/ModuleConfig/Module/FilesGalleriesModuleConfig.php
@@ -3,10 +3,21 @@
 namespace App\Services\ModuleConfig\Module;
 
 use App\Services\ModuleConfig\AbstractModuleConfig;
+use Illuminate\Http\Request;
 
 class FilesGalleriesModuleConfig extends AbstractModuleConfig
 {
     public $Route     = 'filesGalleries';
     public $TableName = 'files_galleries';
     public $Name      = 'Galerias de arquivos';
+
+    public function setNav(Request $request, int $id = null): array
+    {
+        $response[] = [
+            'name'  => 'Principal',
+            'route' => route('filesGalleries.form', ['id' => $id]),
+        ];
+
+        return $response;
+    }
 }
