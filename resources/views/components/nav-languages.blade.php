11f546f0eb6eba13658ad99edc23fa401db4d690
Jhonny Fialho <jhonny@hello-world.cc>
2020-12-29 18:12:34 -0300

    nav Languages

--- resources/views/components/nav-languages.blade.php
+++ resources/views/components/nav-languages.blade.php
@@ -5,7 +5,7 @@
                 @php
                     $route_params['language_id'] = $language->id;
                 @endphp                
-                <a {{ in_array('ajax-item', $class_item) ? 'data-url=' . route($route, $route_params) :  'href=' . route($route, $route_params) }}  class="nav-link {{ implode(' ', $class_item) }} {{ $language_id == $language->id ? 'active' : '' }}">
+                <a {{ in_array('ajax-item', $class_item) ? 'data-url=' . route($route, $route_params) :  'href=' . route($route, $route_params) }} class="nav-link {{ implode(' ', $class_item) }} {{ $language_id == $language->id ? 'active' : '' }}">
                     {{ $language->language }}
                 </a>
             </li>
